e76efbe3b6637cdb50eb97058f8806850ccea538
Agus Prasetiyo <agusprasetiyo27@gmail.com>
2018-01-13 17:52:14 +0700

    finish - 1

---
+++ common/models/Item.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "item".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $price
+ * @property int $category_id
+ */
+class Item extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'item';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['price', 'category_id'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'price' => 'Price',
+            'category_id' => 'Category ID',
+        ];
+    }
+}
---
+++ frontend/views/item/index.php
@@ -0,0 +1,36 @@
+<?php
+/* AGUS PRASETIYO - 2110177018 - D4 LJ PJJ 2017 */
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\ItemSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Items';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="item-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Item', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'price',
+            'category_id',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
